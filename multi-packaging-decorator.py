b1404c6611f926a8b9669383426019aec3892e19
rox-streuli <roxstreuli@gmail.com>
2023-02-26 08:56:53 +0000

    Added files from PyCharm Community Edition 2021.1.2 x64

---
+++ multi-packaging-decorator.py
@@ -0,0 +1,43 @@
+class WarehouseDecorator:
+    def __init__(self, material):
+        self.material = material
+
+    def __call__(self, own_function):
+        def internal_wrapper(*args, **kwargs):
+            print('Wrapping items from {} with {}'.
+                  format(own_function.__name__, self.material))
+            own_function(*args, **kwargs)
+            print()
+        return internal_wrapper
+
+
+# when we call the decorator with args, the function is passed to the __call__
+# method, instead we pass the args to the  __init__ method
+@WarehouseDecorator('kraft')
+def pack_books(*args):
+    print("We'll pack books:", args)
+
+
+@WarehouseDecorator('foil')
+def pack_toys(*args):
+    print("We'll pack toys:", args)
+
+
+@WarehouseDecorator('cardboard')
+def pack_fruits(*args):
+    print("We'll pack fruits:", args)
+
+
+pack_books('Alice in Wonderland', 'Winnie the Pooh')
+pack_toys('doll', 'car')
+pack_fruits('plum', 'pear')
+
+# outputs...
+# Wrapping items from pack_books with kraft
+# We'll pack books: ('Alice in Wonderland', 'Winnie the Pooh')
+#
+# Wrapping items from pack_toys with foil
+# We'll pack toys: ('doll', 'car')
+#
+# Wrapping items from pack_fruits with cardboard
+# We'll pack fruits: ('plum', 'pear')
